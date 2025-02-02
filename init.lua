96dc85f7c947cbb705f1c17c4dd799af181d5f26
KalXD-g <kalyan3x90@gmail.com>
2025-02-02 19:08:14 +0530

    Added nvim-cmp

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 4  -- Indent with 4 spaces
+vim.opt.tabstop = 4      -- Tabs appear as 4 spaces
+vim.opt.softtabstop = 4  -- Backspace treats tab as 4 spaces
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cmdheight = 0
@@ -6,3 +9,4 @@ vim.opt.scrolloff = 5
 vim.opt.cursorline = true
 
 require("config.lazy")
+
