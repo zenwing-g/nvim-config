75b637d24c563d4be2da68eb629b0f97190ab819
KalXD-g <kalyan3x90@gmail.com>
2025-02-03 14:36:23 +0530

    Added indent visualizer (indent_blankline) plugin

---
+++ lua/plugins/indent_blankline.lua
@@ -0,0 +1,10 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {
+        indent = { char = "│" }, -- Customize indent character
+        scope = { enabled = true }, -- Highlight the current scope
+        exclude = { filetypes = { "help", "markdown" } } -- Exclude certain filetypes
+    }
+}
+
