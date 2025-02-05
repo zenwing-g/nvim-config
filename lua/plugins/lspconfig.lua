fa675f80ec5d29946b70ed628f9faae87fc6d007
KalXD-g <kalyan3x90@gmail.com>
2025-02-05 18:02:30 +0530

    Configure LSP with "Go to Definition" and "Signature Help" keybindings
    - Added keybindings for "Go to Definition" and "Signature Help".

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,12 +5,13 @@ return {
         local mason = require("mason")
         local mason_lspconfig = require("mason-lspconfig")
         local lspconfig = require("lspconfig")
+        local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
         -- List of LSP servers to install
         local servers = {
             "pyright",    -- Python LSP
             "clangd",     -- C++ LSP
-            "ruff",       -- Python linter as an LSP (updated from ruff_lsp)
+            "ruff",       -- Python linter as an LSP
         }
 
         -- Setup Mason
@@ -19,9 +20,22 @@ return {
             ensure_installed = servers, -- Ensure these LSPs are installed
         })
 
+        -- Keybindings for LSP features
+        local on_attach = function(client, bufnr)
+            local bufopts = { noremap = true, silent = true, buffer = bufnr }
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts) -- Go to Definition
+            vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, bufopts) -- Show Signature Help
+        end
+
+        -- Add nvim-cmp capabilities
+        local capabilities = cmp_nvim_lsp.default_capabilities()
+
         -- Setup LSP servers automatically
         for _, server in ipairs(servers) do
-            lspconfig[server].setup({})
+            lspconfig[server].setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+            })
         end
 
         -- Custom command to install all Mason tools manually
