39672757880fa4080c267c34bf4d96e2144ef5a2
KalXD-g <kalyan3x90@gmail.com>
2025-02-27 06:44:23 +0530

    minor changes to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,16 +4,15 @@ return {
     config = function()
       require("lualine").setup({
         options = {
-          section_separators = {},  -- Keep section separators
-          component_separators = {},  -- Keep component separators
+          section_separators = "",
+          component_separators = "",
         },
         sections = {
-          lualine_a = { { "filename", path = 2, symbols = { modified = " ", readonly = " " } } },
+          lualine_a = { { "filename", path = 2, symbols = { modified = "[+]", readonly = "[read only]", unnamed = "[No name]" } } },
           lualine_b = { "branch" },
-          lualine_c = { },
           lualine_x = { "diagnostics" },
           lualine_y = { "filesize" },
-          lualine_z = { { "location", separator = { left = "", right = "" }, left_padding = 2 } },
+          lualine_z = { "location" },
         },
       })
     end,
