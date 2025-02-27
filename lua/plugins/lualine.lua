953f1bef52e8cffb5883c47b469c9dfac891e74d
KalXD-g <kalyan3x90@gmail.com>
2025-02-27 06:46:14 +0530

    ignore

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,6 +10,7 @@ return {
         sections = {
           lualine_a = { { "filename", path = 2, symbols = { modified = "[+]", readonly = "[read only]", unnamed = "[No name]" } } },
           lualine_b = { "branch" },
+          lualine_c = { },
           lualine_x = { "diagnostics" },
           lualine_y = { "filesize" },
           lualine_z = { "location" },
