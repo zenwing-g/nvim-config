c010604425fffcd679dfd49bf69f1ce82f681e9b
KalXD-g <kalyan3x90@gmail.com>
2025-03-19 19:21:08 +0530

    removed italics in catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -30,12 +30,6 @@ return {
 			vim.cmd("highlight NormalNC guibg=NONE ctermbg=NONE")
 			vim.cmd("highlight Pmenu guibg=NONE ctermbg=NONE")
 			vim.cmd("highlight PmenuSel guibg=#3c383c ctermbg=NONE")
-
-			-- Apply italics to specific syntax elements
-			vim.cmd("highlight Comment cterm=italic gui=italic")
-			vim.cmd("highlight Keyword cterm=italic gui=italic")
-			vim.cmd("highlight Function cterm=italic gui=italic")
-			vim.cmd("highlight Identifier cterm=italic gui=italic")
 		end,
 	},
 }
